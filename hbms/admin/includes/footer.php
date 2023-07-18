78199768f8a74bd232c0efe7d6f2d32a05a696d3
priyanshuthakar24 <priyanshuthakar24@gmail.com>
2023-07-18 19:35:57 +0530

    initial commit

---
+++ hbms/admin/includes/footer.php
@@ -0,0 +1,7 @@
+<div>
+         
+            <div class="footer">
+                    <div class="clearfix"> </div>
+                        <p style="font-family: cursive;">© Bookrooms </p>
+            </div>
+        </div>
---
+++ hbms/includes/footer.php
@@ -0,0 +1,8 @@
+<div class="footer-section">
+						<div class="container">
+							<div class="footer-top">
+								<p>Bookrooms @ 2023 </p>
+							</div>
+						
+				</div>
+			</div>
